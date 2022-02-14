eb1933234ce5e8e3b45cb772d5792944adc5eacd
Anna Doubkova <anna.doubkova@thrive.health>
2022-02-14 15:18:59 -0800

    Add TF deployment s3 & table

---
+++ terraform/deployment/lock_table.tf
@@ -0,0 +1,19 @@
+resource "aws_dynamodb_table" "lock_table" {
+  name           = "tf-lock-table"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "LockID"
+  tags           = local.common_tags
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+resource "aws_ssm_parameter" "locks_table_arn" {
+  name  = "tf/tf-locks-table-arn"
+  type  = "String"
+  value = aws_dynamodb_table.lock_table.arn
+}
