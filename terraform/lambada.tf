6f2362739339b3bd5ed7266638b329b71b9dda7c
Anna Doubkova <anna.doubkova@thrive.health>
2022-02-14 15:24:56 -0800

    Comment out the lambda role code

--- terraform/lambada.tf
+++ terraform/lambada.tf
@@ -1,36 +1,36 @@
-resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
+# resource "aws_iam_role" "iam_for_lambda" {
+#   name = "iam_for_lambda"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
+#   assume_role_policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Action": "sts:AssumeRole",
+#       "Principal": {
+#         "Service": "lambda.amazonaws.com"
+#       },
+#       "Effect": "Allow",
+#       "Sid": ""
+#     }
+#   ]
+# }
+# EOF
+# }
 
-data "archive_file" "lambda_zip" {
-    type          = "zip"
-    source_file   = "../src/index.js"
-    output_path   = "lambda_function.zip"
-}
+# data "archive_file" "lambda_zip" {
+#     type          = "zip"
+#     source_file   = "../src/index.js"
+#     output_path   = "lambda_function.zip"
+# }
 
-resource "aws_lambda_function" "test_lambda" {
-  filename      = "lambda_function.zip"
-  function_name = "test_lambda"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "index.handler"
+# resource "aws_lambda_function" "test_lambda" {
+#   filename      = "lambda_function.zip"
+#   function_name = "test_lambda"
+#   role          = aws_iam_role.iam_for_lambda.arn
+#   handler       = "index.handler"
 
-  source_code_hash = "${data.archive_file.lambda_zip.output_base64sha256}"
+#   source_code_hash = "${data.archive_file.lambda_zip.output_base64sha256}"
 
-  runtime = "nodejs12.x"
-}
+#   runtime = "nodejs12.x"
+# }
