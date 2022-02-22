34cabd2fd70442c5c97b41a2cf6f80744f8c5885
Anna Doubkova <anna.doubkova@thrive.health>
2022-02-22 10:42:11 -0800

    Add terraform backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,14 @@ terraform {
   }
 
   required_version = ">= 0.14.9"
+
+  backend "s3" {
+    encrypt = true
+    bucket = aws_s3_bucket.remote_state.name
+    dynamodb_table = aws_dynamodb_table.lock_table.name
+    key = "terraform.tfstate"
+    region = "ca-central-1"
+  }
 }
 
 provider "aws" {
