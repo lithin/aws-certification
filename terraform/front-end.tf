7f738502c1dc1f3eb476021f55ff67236a9cce30
Anna Doubkova <anna.doubkova@thrive.health>
2022-03-02 11:38:38 -0800

    Amend distro

--- terraform/front-end.tf
+++ terraform/front-end.tf
@@ -21,12 +21,28 @@ resource "aws_cloudfront_distribution" "front_end" {
     domain_name = aws_s3_bucket.front_end.bucket_regional_domain_name
 
     s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.default
+      origin_access_identity = aws_cloudfront_origin_access_identity.default.cloudfront_access_identity_path
     }
   }
 
   default_cache_behavior {
     target_origin_id = "frontend"
+
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
   }
 }
 
