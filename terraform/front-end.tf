c5c200630a6711f537dbb2127d329eec429fcc24
Anna Doubkova <anna.doubkova@thrive.health>
2022-03-04 18:14:32 -0800

    Comment stuff out

--- terraform/front-end.tf
+++ terraform/front-end.tf
@@ -1,92 +1,92 @@
-resource "aws_cloudfront_distribution" "front_end" {
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "index.html"
-  price_class         = "PriceClass_100"
+# resource "aws_cloudfront_distribution" "front_end" {
+#   enabled             = true
+#   is_ipv6_enabled     = true
+#   default_root_object = "index.html"
+#   price_class         = "PriceClass_100"
 
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
+#   viewer_certificate {
+#     cloudfront_default_certificate = true
+#   }
 
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
+#   restrictions {
+#     geo_restriction {
+#       restriction_type = "none"
+#     }
+#   }
 
-  origin {
-    # https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-cloudfront-distribution-origin.html
+#   origin {
+#     # https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-cloudfront-distribution-origin.html
 
-    origin_id   = "frontend" # must be unique within distribution
-    domain_name = aws_s3_bucket.front_end.bucket_regional_domain_name
+#     origin_id   = "frontend" # must be unique within distribution
+#     domain_name = aws_s3_bucket.front_end.bucket_regional_domain_name
 
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.default.cloudfront_access_identity_path
-    }
-  }
+#     s3_origin_config {
+#       origin_access_identity = aws_cloudfront_origin_access_identity.default.cloudfront_access_identity_path
+#     }
+#   }
 
-  default_cache_behavior {
-    target_origin_id = "frontend"
+#   default_cache_behavior {
+#     target_origin_id = "frontend"
 
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
+#     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+#     cached_methods   = ["GET", "HEAD"]
 
-    forwarded_values {
-      query_string = false
+#     forwarded_values {
+#       query_string = false
 
-      cookies {
-        forward = "none"
-      }
-    }
+#       cookies {
+#         forward = "none"
+#       }
+#     }
 
-    viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-}
+#     viewer_protocol_policy = "allow-all"
+#     min_ttl                = 0
+#     default_ttl            = 3600
+#     max_ttl                = 86400
+#   }
+# }
 
-resource "aws_cloudfront_origin_access_identity" "default" {}
+# resource "aws_cloudfront_origin_access_identity" "default" {}
 
 
-# s3
+# # s3
 
-resource "aws_s3_bucket" "front_end" {
-  bucket = "anna-aws-certification-front-end"
-  acl    = "public-read"
+# resource "aws_s3_bucket" "front_end" {
+#   bucket = "anna-aws-certification-front-end"
+#   acl    = "public-read"
 
-  cors_rule {
-    allowed_headers = ["Authorization", "Content-Length"]
-    allowed_methods = ["GET", "POST"]
-    allowed_origins = ["https://cloudfront.net"]
-    max_age_seconds = 3000
-  }
+#   cors_rule {
+#     allowed_headers = ["Authorization", "Content-Length"]
+#     allowed_methods = ["GET", "POST"]
+#     allowed_origins = ["https://cloudfront.net"]
+#     max_age_seconds = 3000
+#   }
 
-  website {
-    index_document = "index.html"
-  }
+#   website {
+#     index_document = "index.html"
+#   }
 
-  tags = {
-    group = "Frontend"
-  }
-}
+#   tags = {
+#     group = "Frontend"
+#   }
+# }
 
-resource "aws_s3_bucket_policy" "front_end" {
-  bucket = aws_s3_bucket.front_end.id
+# resource "aws_s3_bucket_policy" "front_end" {
+#   bucket = aws_s3_bucket.front_end.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource = [
-          aws_s3_bucket.front_end.arn,
-          "${aws_s3_bucket.front_end.arn}/*",
-        ]
-      },
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Sid       = "PublicReadGetObject"
+#         Effect    = "Allow"
+#         Principal = "*"
+#         Action    = "s3:GetObject"
+#         Resource = [
+#           aws_s3_bucket.front_end.arn,
+#           "${aws_s3_bucket.front_end.arn}/*",
+#         ]
+#       },
+#     ]
+#   })
+# }
